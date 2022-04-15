3b9ff9c4f24282531c300153f3a16caf07b5fac0
DESKTOP-T7VO2G2\Sadicoman <francois.szcz@hotmail.fr>
2022-04-15 21:45:37 +0200

    Update BTN
    Mise a jour des bouton et actualisation de ceux si

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -23,14 +23,23 @@ let btnTaille = document.querySelectorAll(".btn");
 let btnTailleHaut = document.querySelectorAll(".btn__el--haut");
 let btnTailleBas = document.querySelectorAll(".btn__el--bas");
 
-for(let i = 0 ; i < btnTaille.length ; i++){ 
-	// .length => nombre d'éléments dans le tableau "btnTaille"
-	let btnHeight = btnTailleHaut[i].offsetHeight;
-	let btnWidth = btnTailleHaut[i].offsetWidth;
+function updateStyleBtn(){
+  for(let i = 0 ; i < btnTaille.length ; i++){ 
+    // .length => nombre d'éléments dans le tableau "btnTaille"
+    let btnHeight = btnTailleHaut[i].offsetHeight;
+    let btnWidth = btnTailleHaut[i].offsetWidth;
+    if(btnTaille[i].classList.contains("btn--icon")){
+      // btnWidth += 15;
+    }
+    
+    btnTailleBas[i].style.width = btnWidth+"px";
+    btnTailleBas[i].style.height = btnHeight+"px";
+  };
+}
 
-	btnTailleBas[i].style.width = btnWidth+"px";
-	btnTailleBas[i].style.height = btnHeight+"px";
-};
+// mise à jour de la taille toutes les X millisecondes
+let interval = setInterval(updateStyleBtn, 100);
+interval;
 /*--------------------------------------------*/
 
 /*----- Section Swap -----*/
